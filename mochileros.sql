acf4df811dc200c09e7b3d134bc3465df5ded574
Daniel10024 <DanielGaleano2011@gmail.com>
2018-09-28 02:54:25 -0300

    base de datos

---
+++ mochileros.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-09-2018 a las 07:52:03
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mochileros`
+--
+CREATE DATABASE IF NOT EXISTS `mochileros` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `mochileros`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `punto`
+--
+
+CREATE TABLE `punto` (
+  `ID_Punto` int(11) NOT NULL,
+  `Fecha_inicio` date NOT NULL,
+  `Fecha_fin` date NOT NULL,
+  `Lugar` varchar(64) NOT NULL,
+  `Intereses` varchar(64) NOT NULL,
+  `ID_Viaje` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `punto`:
+--   `ID_Viaje`
+--       `viaje` -> `ID_Viaje`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID_Usuario` int(11) NOT NULL,
+  `Nombre` varchar(64) NOT NULL,
+  `Apellido` varchar(64) NOT NULL,
+  `Edad` int(11) DEFAULT NULL,
+  `Idioma` varchar(64) DEFAULT NULL,
+  `Pais` varchar(64) DEFAULT NULL,
+  `Intereses` varchar(64) DEFAULT NULL,
+  `Contacto` varchar(64) DEFAULT NULL,
+  `Descripcion` varchar(320) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `usuario`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje`
+--
+
+CREATE TABLE `viaje` (
+  `ID_Viaje` int(11) NOT NULL,
+  `ID_Usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `viaje`:
+--   `ID_Usuario`
+--       `usuario` -> `ID_Usuario`
+--
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `punto`
+--
+ALTER TABLE `punto`
+  ADD PRIMARY KEY (`ID_Punto`),
+  ADD KEY `ID_Viaje` (`ID_Viaje`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID_Usuario`);
+
+--
+-- Indices de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD PRIMARY KEY (`ID_Viaje`),
+  ADD KEY `ID_Usuario` (`ID_Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `punto`
+--
+ALTER TABLE `punto`
+  MODIFY `ID_Punto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  MODIFY `ID_Viaje` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `punto`
+--
+ALTER TABLE `punto`
+  ADD CONSTRAINT `punto_ibfk_1` FOREIGN KEY (`ID_Viaje`) REFERENCES `viaje` (`ID_Viaje`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD CONSTRAINT `viaje_ibfk_1` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuario` (`ID_Usuario`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
