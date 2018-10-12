722fa085ec857ce2b896d4a216c4e31c37750cfe
romanvenica <romanvenica@gmail.com>
2018-10-12 16:29:53 -0300

    Cambios de disenio

--- p3.php
+++ p3.php
@@ -128,7 +128,7 @@ if(!isset($_SESSION["‘ID_user’"])) {
 
 </div>
           
-        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3736489.7218514383!2d90.21589792292741!3d23.857125486636733!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1506502314230" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3736489.7218514383!2d90.21589792292741!3d23.857125486636733!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1506502314230" width="90%" height="315" frameborder="0" style="border:0; left: 5%; position: relative; border-style: solid; border-width: 1px; border-color: gray;" allowfullscreen></iframe>
       <div class="container">
       <div class="row">
         <div class="col-xs-6">
