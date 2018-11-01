68826f55f2f5f91a4bebf869b4854dc5fc1f4c6e
Roman <romanvenica@gmail.com>
2018-11-01 10:05:58 -0300

    bdd agregada

--- mochileros.sql
+++ mochileros.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-10-2018 a las 04:42:21
+-- Tiempo de generación: 01-11-2018 a las 14:04:27
 -- Versión del servidor: 10.1.28-MariaDB
--- Versión de PHP: 7.1.10
+-- Versión de PHP: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,72 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `mochileros`
 --
-CREATE DATABASE IF NOT EXISTS `mochileros` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mochileros`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `amigo`
+--
+
+CREATE TABLE `amigo` (
+  `ID_USUARIO` bigint(20) NOT NULL,
+  `ID_USUARIO_1` bigint(20) NOT NULL,
+  `ESTADO` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `escala`
+--
+
+CREATE TABLE `escala` (
+  `ID_ESCALA` bigint(20) NOT NULL,
+  `ESCALA` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `escala`
+--
+
+INSERT INTO `escala` (`ID_ESCALA`, `ESCALA`) VALUES
+(1, 'america');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habla`
+--
+
+CREATE TABLE `habla` (
+  `ID_IDIOMA` bigint(20) NOT NULL,
+  `ID_USUARIO` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `habla`
+--
+
+INSERT INTO `habla` (`ID_IDIOMA`, `ID_USUARIO`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `idiomas`
+--
+
+CREATE TABLE `idiomas` (
+  `ID_IDIOMA` bigint(20) NOT NULL,
+  `IDIOMA` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `idiomas`
+--
+
+INSERT INTO `idiomas` (`ID_IDIOMA`, `IDIOMA`) VALUES
+(1, 'geringoso');
 
 -- --------------------------------------------------------
 
@@ -31,54 +95,22 @@ USE `mochileros`;
 --
 
 CREATE TABLE `interes` (
-  `ID_Interes` int(11) NOT NULL,
-  `ID_Punto` int(11) NOT NULL,
-  `Nombre` varchar(64) NOT NULL
+  `ID_INTERES` bigint(20) NOT NULL,
+  `DESCRIPCION` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELACIONES PARA LA TABLA `interes`:
---   `ID_Punto`
---       `punto` -> `ID_Punto`
---
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `publicacion`
+-- Estructura de tabla para la tabla `le_interesa`
 --
 
-CREATE TABLE `publicacion` (
-  `ID_Publicacion` int(11) NOT NULL,
-  `ID_Usuario` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Comentario` varchar(640) NOT NULL,
-  `Publico` int(11) NOT NULL,
-  `Fecha` date NOT NULL
+CREATE TABLE `le_interesa` (
+  `ID_PUNTO` bigint(20) NOT NULL,
+  `ID_VIAJE` bigint(20) NOT NULL,
+  `ID_USUARIO` bigint(20) NOT NULL,
+  `ID_INTERES` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELACIONES PARA LA TABLA `publicacion`:
---   `ID_Usuario`
---       `usuario` -> `ID_Usuario`
---
-
---
--- Volcado de datos para la tabla `publicacion`
---
-
-INSERT INTO `publicacion` (`ID_Publicacion`, `ID_Usuario`, `Comentario`, `Publico`, `Fecha`) VALUES
-(1, '110433993825937047664', 'esto es un comentario (esto esta publico)', 2, '2018-10-18'),
-(8, '117974607496192147045', 'comento esto para probar (esto esta publico)', 2, '2018-10-18'),
-(9, '111733596368903726939', 'probamos esto con un texto un poco mas largo.\r\nprobamos esto con un texto un poco mas largo.\r\nprobamos esto con un texto un poco mas largo.\r\nprobamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largoprobamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.probamos esto con un texto un poco mas largo.(esto esta publico)', 2, '2018-10-18'),
-(10, '117974607496192147045', 'esto solo lo veo yo y mis amigos :D', 1, '2018-10-18'),
-(16, '117974607496192147045', 'sad', 1, '2018-10-21'),
-(17, '117974607496192147045', 'esto es publico', 1, '2018-10-21'),
-(18, '117974607496192147045', 'esto si es publico XD', 2, '2018-10-21'),
-(19, '117974607496192147045', 'sad', 1, '2018-10-22'),
-(20, '117974607496192147045', 'ahora', 1, '2018-10-22'),
-(21, '117974607496192147045', 'esto ya publica bien :D', 1, '2018-10-22'),
-(22, '117974607496192147045', 'si es publico tambien publica bien?', 2, '2018-10-22'),
-(23, '111733596368903726939', '', 1, '2018-10-22');
 
 -- --------------------------------------------------------
 
@@ -87,46 +119,22 @@ INSERT INTO `publicacion` (`ID_Publicacion`, `ID_Usuario`, `Comentario`, `Public
 --
 
 CREATE TABLE `punto` (
-  `ID_Punto` int(11) NOT NULL,
-  `Fecha_inicio` date NOT NULL,
-  `Fecha_fin` date NOT NULL,
-  `Cordenadas` varchar(64) NOT NULL,
-  `Intereses` varchar(64) NOT NULL,
-  `ID_Viaje` int(11) NOT NULL
+  `ID_PUNTO` bigint(20) NOT NULL,
+  `ID_VIAJE` bigint(20) NOT NULL,
+  `ID_USUARIO` bigint(20) NOT NULL,
+  `EJE_X` bigint(20) NOT NULL,
+  `EJE_Y` bigint(20) NOT NULL,
+  `FECHA_INICIO` datetime NOT NULL,
+  `FECHA_FIN` date NOT NULL,
+  `RADIO_EXTRA` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- RELACIONES PARA LA TABLA `punto`:
---   `ID_Viaje`
---       `viaje` -> `ID_Viaje`
+-- Volcado de datos para la tabla `punto`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `solisitud`
---
-
-CREATE TABLE `solisitud` (
-  `ID_solisitud` int(11) NOT NULL,
-  `User` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Amigo` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Estado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELACIONES PARA LA TABLA `solisitud`:
---
-
---
--- Volcado de datos para la tabla `solisitud`
---
-
-INSERT INTO `solisitud` (`ID_solisitud`, `User`, `Amigo`, `Estado`) VALUES
-(6, '110433993825937047664', '111733596368903726939', 2),
-(8, '117974607496192147045', '110433993825937047664', 1),
-(9, '117974607496192147045', '111733596368903726939', 2),
-(10, '107533581959836595008', '111733596368903726939', 2);
+INSERT INTO `punto` (`ID_PUNTO`, `ID_VIAJE`, `ID_USUARIO`, `EJE_X`, `EJE_Y`, `FECHA_INICIO`, `FECHA_FIN`, `RADIO_EXTRA`) VALUES
+(3, 2, 1, 5, 7, '2018-11-13 00:00:00', '2018-12-12', 15);
 
 -- --------------------------------------------------------
 
@@ -135,121 +143,143 @@ INSERT INTO `solisitud` (`ID_solisitud`, `User`, `Amigo`, `Estado`) VALUES
 --
 
 CREATE TABLE `usuario` (
-  `ID_Usuario` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Nombre` varchar(64) NOT NULL,
-  `Apellido` varchar(64) NOT NULL,
-  `Edad` date DEFAULT NULL,
-  `Idioma` varchar(64) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
-  `Pais` varchar(64) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
-  `Intereses` varchar(64) DEFAULT NULL,
-  `Contacto` varchar(64) DEFAULT NULL,
-  `Descripcion_U` varchar(320) DEFAULT NULL
+  `ID_USUARIO` bigint(20) NOT NULL,
+  `NOMBRE` char(16) NOT NULL,
+  `APELLIDO` char(16) NOT NULL,
+  `FECHA_NAC` datetime NOT NULL,
+  `PAIS` char(16) NOT NULL,
+  `DESCRIPCION` varchar(255) NOT NULL,
+  `CONTACTO` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELACIONES PARA LA TABLA `usuario`:
---
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`ID_Usuario`, `Nombre`, `Apellido`, `Edad`, `Idioma`, `Pais`, `Intereses`, `Contacto`, `Descripcion_U`) VALUES
-('1', 'Invitado', ' ', '0000-00-00', '', '', '', '', ''),
-('107533581959836595008', 'daniel', 'galeano', NULL, NULL, NULL, NULL, NULL, NULL),
-('110433993825937047664', 'daniel', 'galeano', NULL, NULL, NULL, NULL, NULL, NULL),
-('111733596368903726939', 'pablo', 'kimon', NULL, NULL, NULL, NULL, NULL, NULL),
-('117974607496192147045', 'Daniel', 'Galeano', '1995-08-24', '', 'Argentina', '', '', '');
+INSERT INTO `usuario` (`ID_USUARIO`, `NOMBRE`, `APELLIDO`, `FECHA_NAC`, `PAIS`, `DESCRIPCION`, `CONTACTO`) VALUES
+(1, 'ezequiel', 'antencho', '2017-10-18 00:00:00', 'argentina', 'Soy un chico normal, bastante fachero, pero con dificultades a la hora de socializar.', '15000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje`
+--
+
+CREATE TABLE `viaje` (
+  `ID_VIAJE` bigint(20) NOT NULL,
+  `ID_USUARIO` bigint(20) NOT NULL,
+  `ID_ESCALA` bigint(20) DEFAULT NULL,
+  `NOMBRE` char(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `viaje`
+--
+
+INSERT INTO `viaje` (`ID_VIAJE`, `ID_USUARIO`, `ID_ESCALA`, `NOMBRE`) VALUES
+(2, 1, 1, 'El viaje de ronaldo');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `amigo`
+--
+ALTER TABLE `amigo`
+  ADD PRIMARY KEY (`ID_USUARIO`,`ID_USUARIO_1`),
+  ADD KEY `ID_USUARIO_1` (`ID_USUARIO_1`);
+
+--
+-- Indices de la tabla `escala`
+--
+ALTER TABLE `escala`
+  ADD PRIMARY KEY (`ID_ESCALA`);
+
+--
+-- Indices de la tabla `habla`
+--
+ALTER TABLE `habla`
+  ADD PRIMARY KEY (`ID_IDIOMA`,`ID_USUARIO`),
+  ADD KEY `ID_USUARIO` (`ID_USUARIO`);
+
+--
+-- Indices de la tabla `idiomas`
+--
+ALTER TABLE `idiomas`
+  ADD PRIMARY KEY (`ID_IDIOMA`);
+
+--
 -- Indices de la tabla `interes`
 --
 ALTER TABLE `interes`
-  ADD PRIMARY KEY (`ID_Interes`),
-  ADD KEY `ID_Punto` (`ID_Punto`);
+  ADD PRIMARY KEY (`ID_INTERES`);
 
 --
--- Indices de la tabla `publicacion`
+-- Indices de la tabla `le_interesa`
 --
-ALTER TABLE `publicacion`
-  ADD PRIMARY KEY (`ID_Publicacion`),
-  ADD KEY `ID_Usuario` (`ID_Usuario`),
-  ADD KEY `ID_Usuario_2` (`ID_Usuario`),
-  ADD KEY `ID_Usuario_3` (`ID_Usuario`);
+ALTER TABLE `le_interesa`
+  ADD PRIMARY KEY (`ID_PUNTO`,`ID_VIAJE`,`ID_USUARIO`,`ID_INTERES`),
+  ADD KEY `ID_INTERES` (`ID_INTERES`);
 
 --
 -- Indices de la tabla `punto`
 --
 ALTER TABLE `punto`
-  ADD PRIMARY KEY (`ID_Punto`),
-  ADD KEY `ID_Viaje` (`ID_Viaje`);
-
---
--- Indices de la tabla `solisitud`
---
-ALTER TABLE `solisitud`
-  ADD PRIMARY KEY (`ID_solisitud`),
-  ADD KEY `User` (`User`);
+  ADD PRIMARY KEY (`ID_PUNTO`,`ID_VIAJE`,`ID_USUARIO`),
+  ADD KEY `ID_VIAJE` (`ID_VIAJE`,`ID_USUARIO`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`ID_Usuario`);
+  ADD PRIMARY KEY (`ID_USUARIO`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indices de la tabla `viaje`
 --
-
---
--- AUTO_INCREMENT de la tabla `interes`
---
-ALTER TABLE `interes`
-  MODIFY `ID_Interes` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `publicacion`
---
-ALTER TABLE `publicacion`
-  MODIFY `ID_Publicacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
--- AUTO_INCREMENT de la tabla `punto`
---
-ALTER TABLE `punto`
-  MODIFY `ID_Punto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `solisitud`
---
-ALTER TABLE `solisitud`
-  MODIFY `ID_solisitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `viaje`
+  ADD PRIMARY KEY (`ID_VIAJE`,`ID_USUARIO`),
+  ADD KEY `ID_USUARIO` (`ID_USUARIO`),
+  ADD KEY `ID_ESCALA` (`ID_ESCALA`);
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `interes`
+-- Filtros para la tabla `amigo`
 --
-ALTER TABLE `interes`
-  ADD CONSTRAINT `interes_ibfk_1` FOREIGN KEY (`ID_Punto`) REFERENCES `punto` (`ID_Punto`) ON UPDATE CASCADE;
+ALTER TABLE `amigo`
+  ADD CONSTRAINT `amigo_ibfk_1` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID_USUARIO`),
+  ADD CONSTRAINT `amigo_ibfk_2` FOREIGN KEY (`ID_USUARIO_1`) REFERENCES `usuario` (`ID_USUARIO`);
 
 --
--- Filtros para la tabla `publicacion`
+-- Filtros para la tabla `habla`
 --
-ALTER TABLE `publicacion`
-  ADD CONSTRAINT `publicacion_ibfk_1` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuario` (`ID_Usuario`);
+ALTER TABLE `habla`
+  ADD CONSTRAINT `habla_ibfk_1` FOREIGN KEY (`ID_IDIOMA`) REFERENCES `idiomas` (`ID_IDIOMA`),
+  ADD CONSTRAINT `habla_ibfk_2` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID_USUARIO`);
+
+--
+-- Filtros para la tabla `le_interesa`
+--
+ALTER TABLE `le_interesa`
+  ADD CONSTRAINT `le_interesa_ibfk_1` FOREIGN KEY (`ID_PUNTO`,`ID_VIAJE`,`ID_USUARIO`) REFERENCES `punto` (`ID_PUNTO`, `ID_VIAJE`, `ID_USUARIO`),
+  ADD CONSTRAINT `le_interesa_ibfk_2` FOREIGN KEY (`ID_INTERES`) REFERENCES `interes` (`ID_INTERES`);
 
 --
 -- Filtros para la tabla `punto`
 --
 ALTER TABLE `punto`
-  ADD CONSTRAINT `punto_ibfk_1` FOREIGN KEY (`ID_Viaje`) REFERENCES `viaje` (`ID_Viaje`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `punto_ibfk_1` FOREIGN KEY (`ID_VIAJE`,`ID_USUARIO`) REFERENCES `viaje` (`ID_VIAJE`, `ID_USUARIO`);
+
+--
+-- Filtros para la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD CONSTRAINT `viaje_ibfk_1` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID_USUARIO`),
+  ADD CONSTRAINT `viaje_ibfk_2` FOREIGN KEY (`ID_ESCALA`) REFERENCES `escala` (`ID_ESCALA`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
