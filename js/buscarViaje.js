038df2bb6186103596190ae9030f1eadda4ffb03
Roman <romanvenica@gmail.com>
2018-11-16 17:14:37 -0300

    Implouuuuuuud puuummmm!

--- js/buscarViaje.js
+++ js/buscarViaje.js
@@ -2,13 +2,18 @@ $(document).ready(function() {
 	var escala = 1;
 	var origen = "argentina";
 	var idioma = 1;
-	var fecha_ini = new Date(2020,12,27);
-	var fecha_fin = new Date(2020,12,20);
+	var fecha_in = new Date(2018,11,4);
+	var fecha_fi = new Date(2018,12,20);
+	var fecha_ini = fecha_in.getFullYear() + "/" +(fecha_in.getMonth()+1) + "/" + fecha_in.getDate();
+	var fecha_fin = fecha_fi.getFullYear() + "/" +(fecha_fi.getMonth()+1) + "/" + fecha_fi.getDate();
+	var intereses = [1];
+	console.log(fecha_ini);
+	console.log(fecha_fin);
 	$.ajax({
     type: "POST",
 	url: "php/buscarViaje.php",
 	dataType: "json",
-	data: {'escala' : escala, 'fecha_ini' : fecha_ini, 'fecha_fin' : fecha_fin, 'idioma' : idioma, 'origen' :origen},
+	data: {'escala' : escala, 'fecha_ini' : fecha_ini, 'fecha_fin' : fecha_fin, 'idioma' : idioma, 'origen' :origen, 'intereses' : intereses},
 	success: function (result) 
 		{
 			console.log(result);
--- php/buscarViaje.php
+++ php/buscarViaje.php
@@ -10,20 +10,23 @@ try
 
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+        $intereses = $_POST['intereses'];
         // prepare sql and bind parameters
         $stmt = $conn->prepare("SELECT p.*, v.id_escala FROM viaje AS v
             JOIN punto AS p ON v.id_viaje = p.id_viaje
             JOIN usuario AS u ON v.id_usuario = u.id_usuario
             JOIN habla AS h ON u.id_usuario = h.id_usuario
-            WHERE ((v.id_escala = :escala) AND (u.pais = :origen AND h.id_idioma = :idioma)
-            AND ((p.fecha_inicio BETWEEN :fecha_ini and :fecha_fin) OR (p.fecha_fin BETWEEN :fecha_ini and :fecha_fin))");
+            JOIN le_interesa AS i ON i.id_punto = p.id_punto
+            WHERE (v.id_escala = :escala) AND (u.pais = :origen AND h.id_idioma = :idioma)
+            AND (p.fecha_inicio BETWEEN :fecha_ini AND :fecha_fin OR p.fecha_fin BETWEEN :fecha_ini AND :fecha_fin)
+            AND i.id_interes IN (".implode(',',$intereses).")");
         
         $escala = $_POST['escala'];
         $fecha_ini = $_POST['fecha_ini'];
         $fecha_fin = $_POST['fecha_fin'];
         $idioma = $_POST['idioma'];
         $origen = $_POST['origen'];
+        $intereses = $_POST['intereses'];
 
         //$intervalo = new DateInterval('P1D');
         //$rangoFechas = new DatePeriod($fecha_ini, $intervalo ,$fecha_fin);
